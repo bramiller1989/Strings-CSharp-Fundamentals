c663096eff567b0a08e1d1de186956470145be18
bramiller1989 <bramiller1989@gmail.com>
2021-01-19 20:16:04 -0600

    add a really hard to read string

--- Program.cs
+++ Program.cs
@@ -10,12 +10,17 @@ namespace Strings
             var lastName = "Smith";
 
             var fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
 
             var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
-
+            Console.WriteLine(myFullName
+            );
             var names = new string[3] {"John", "Jack", "Mary"};
             var formattedNames = string.Join(",", names);
             Console.WriteLine(formattedNames);
+
+            var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
         }
     }
 }

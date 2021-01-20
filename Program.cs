c0034ef4aaf25769cc232a3ceede728e5823b3be
bramiller1989 <bramiller1989@gmail.com>
2021-01-19 20:13:57 -0600

    add different ways to make strings

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-           
+            var firstName = "John";
+            var lastName = "Smith";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
         }
     }
 }
